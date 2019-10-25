4c9b066f36128aeb46531c665913098dc4f87e86
divyendu dutta <connect2divyendu@gmail.com>
2019-10-26 00:23:47 +0530

    fixing RTD build v2.0

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+#Mocking bs4 and selenium to pass builds on RTD
+autodoc_mock_imports  = ['bs4', 'selenium', 'matplotlib']
